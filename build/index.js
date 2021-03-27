3a4b4bc09b5fc74e5d6a5f8a26f9cb7f453e8acb
DavePlayer <daveplayer2001@gmail.com>
2021-03-27 23:28:41 +0100

    setup basic electron env

--- build/index.js
+++ build/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('works');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = __importDefault(require("electron"));
+var url_1 = __importDefault(require("url"));
+var path_1 = __importDefault(require("path"));
+var app = electron_1.default.app, BrowserWindow = electron_1.default.BrowserWindow;
+app.on('ready', function () {
+    var window = new BrowserWindow({});
+    window.loadURL(url_1.default.format({
+        pathname: path_1.default.join(__dirname, '../electron_files/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+});
+console.log(path_1.default.join(__dirname, '/electron_files/index.html'));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 
-console.log('works')
+import electron from 'electron'
+import url from 'url'
+import path from 'path'
+
+const {app, BrowserWindow} = electron
+
+app.on('ready', () => {
+    const window:electron.BrowserWindow = new BrowserWindow({})
+
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, '../electron_files/index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+})
+    console.log(path.join(__dirname, '/electron_files/index.html'))
